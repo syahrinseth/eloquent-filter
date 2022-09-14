5a61ed3e1b94f3f393a36588dfb1507c9f044083
syahrinseth <syahrinseth@users.noreply.github.com>
2022-09-14 10:36:25 +0000

    Fix styling

--- src/EloquentFilter.php
+++ src/EloquentFilter.php
@@ -2,12 +2,11 @@
 
 namespace SyahrinSeth\EloquentFilter;
 
-use SyahrinSeth\EloquentFilter\QueryFilter;
 use Illuminate\Contracts\Database\Query\Builder;
 
 trait EloquentFilter
 {
-    public static function scopeFilter(Builder $query, QueryFilter $filter) : Builder
+    public static function scopeFilter(Builder $query, QueryFilter $filter): Builder
     {
         return $filter->apply($query);
     }
--- src/QueryFilter.php
+++ src/QueryFilter.php
@@ -2,9 +2,9 @@
 
 namespace SyahrinSeth\EloquentFilter;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Eloquent\Builder;
 
 abstract class QueryFilter
 {
@@ -16,51 +16,44 @@ abstract class QueryFilter
 
     /**
      * QueryFilter constructor.
-     * 
-     * @param Request $request
+     *
+     * @param  Request  $request
      */
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
-    public function apply(Builder $builder) : Builder
+    public function apply(Builder $builder): Builder
     {
         $this->builder = $builder;
         $this->filterable = $filterable ?? $this->getTableColumns();
 
         foreach ($this->filters() as $name => $value) {
-
             // Get list of columns
-            if(in_array($name, $this->filterable)) {
-
-                if (method_exists($this, $name) ) {
-
+            if (in_array($name, $this->filterable)) {
+                if (method_exists($this, $name)) {
                     call_user_func_array([$this, $name], array_filter([$value]));
-    
                 } else {
-    
                     $this->builder->orWhere($name, 'LIKE', "%{$value}%");
-    
                 }
-
             }
         }
 
         return $this->builder;
     }
 
-    public function filters() : array
+    public function filters(): array
     {
         return $this->request->all();
     }
 
     /**
      * Get table columns
-     * 
+     *
      * @return array
      */
-    protected function getTableColumns() : array
+    protected function getTableColumns(): array
     {
         $columns = Schema::getColumnListing($this->builder->getModel()->getTable());
 
@@ -69,10 +62,10 @@ abstract class QueryFilter
 
     /**
      * Get table name.
-     * 
+     *
      * @return string
      */
-    protected function getTableName() : string
+    protected function getTableName(): string
     {
         return $this->builder->getModel()->getTable();
     }
@@ -82,6 +75,7 @@ abstract class QueryFilter
         if ($this->request->has(['sort_direction'])) {
             return $this->builder->orderBy($field, $this->request->input('sort_direction') ?? 'asc');
         }
+
         return $this->builder;
     }
 
@@ -89,22 +83,14 @@ abstract class QueryFilter
     {
         $filterable = $this->filterable;
 
-        return $this->builder->where(function($query) use ($input, $filterable) {
-
-            for ($i = 0; count($filterable) > 0;$i++) {
-
+        return $this->builder->where(function ($query) use ($input, $filterable) {
+            for ($i = 0; count($filterable) > 0; $i++) {
                 if ($i == 0) {
-
                     $query->where($filterable[$i], 'like', "%{$input}%");
-                
                 } else {
-                
                     $query->orWhere($filterable[$i], 'like', "%{$input}%");
-                
                 }
-
             }
-
         });
     }
 }
